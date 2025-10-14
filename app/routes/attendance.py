1933ac7f21f96c625a85fbd789223898cc6486a1
Rodolfo Noguez <rodolfonoguez10@gmail.com>
2025-10-14 09:09:21 -0500

    student dashboard complete

--- app/routes/attendance.py
+++ app/routes/attendance.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, session
 
-attendance_bp = Blueprint('attendance', __name__, url_prefix='/attendance')
+attendance_bp = Blueprint('attendance', __name__)  # <- match the import in __init__.py
 
-@attendance_bp.route('/')
-def dashboard():
-    return render_template('dashboard.html')
+@attendance_bp.route('/dashboard/student')
+def student_dashboard():
+    return render_template('dashboard_student.html')
+
+@attendance_bp.route('/dashboard/teacher')
+def teacher_dashboard():
+    return render_template('dashboard_teacher.html')
