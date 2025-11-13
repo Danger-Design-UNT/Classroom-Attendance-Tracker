7d98c2c0fce79ae2bdb85dff25195bf81b409a29
Rodolfo Noguez <rodolfonoguez10@gmail.com>
2025-11-12 22:22:15 -0600

    finished adding limiter to login guesses

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,41 @@
-from flask import Flask
+from flask import Flask, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+limiter = Limiter(key_func=get_remote_address)
 
 def create_app():
+    load_dotenv()
+
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key_here'
 
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    db_path = os.path.join(basedir, '../instance/attendance.db')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+    # Resolve absolute path to instance folder
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    db_path = os.path.join(base_dir, '..', 'instance', 'attendance.db')
 
-    app.config['SESSION_COOKIE_SECURE'] = False
-    app.config['REMEMBER_COOKIE_HTTPONLY'] = True
-    app.config['SESSION_PROTECTION'] = 'strong'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'fallback_key')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+
+
+    app.config.update(
+        SESSION_COOKIE_SECURE=True,
+        SESSION_COOKIE_HTTPONLY=True,
+        SESSION_COOKIE_SAMESITE='Lax',
+        REMEMBER_COOKIE_HTTPONLY=True,
+        SESSION_PROTECTION='strong'
+    )
 
     db.init_app(app)
     login_manager.init_app(app)
+    limiter.init_app(app)
 
     from .models import User
 
@@ -29,10 +44,14 @@ def create_app():
         return User.query.get(int(user_id))
 
     from .routes.auth import auth_bp
-    app.register_blueprint(auth_bp)
-
     from .routes.attendance import attendance_bp
+    app.register_blueprint(auth_bp)
     app.register_blueprint(attendance_bp)
 
+    @app.before_request
+    def enforce_https():
+        if not app.debug and not request.is_secure:
+            url = request.url.replace("http://", "https://", 1)
+            return redirect(url, code=301)
 
     return app
